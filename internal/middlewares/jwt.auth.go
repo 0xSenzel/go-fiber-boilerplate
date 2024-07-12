9d66df67bd4a7826f70733a43751a1e0b03c0794
0xsenzel <shaoqingwong@gmail.com>
2024-07-12 15:18:01 +0800

    jwt generate token and validate token

--- internal/middlewares/jwt.auth.go
+++ internal/middlewares/jwt.auth.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"log"
+	"strings"
 	"time"
 
 	"github.com/0xsenzel/go-fiber-boilerplate/internal/services/user/tables"
@@ -9,28 +10,42 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func JwtAuth() fiber.Handler {
-	return func(c fiber.Ctx) error {
-		// authHeader := c.Get("Authorization")
-		// if authHeader == "" {
-		// 	return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-		// 		"message": "Missing or malformed JWT",
-		// 	})
-		// }
-
-		// tokenString := authHeader[len("Bearer ")]
-		token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{})
-		signedToken, err := token.SignedString(token)
-		if err!= nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map {
-					"error": err.Error(),
-					"message": "Failed to sign access token",
-				})
-		}
-
-		return c.JSON(fiber.Map{
-			"access_token": signedToken,
+func JwtAuth(c fiber.Ctx) error {
+	clientToken := c.Get("Authorization")
+	if clientToken == "" {
+		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "No Authorization header provided",
 		})
+	}
+	
+	// remove "Bearer" prefix if exists
+	splitToken := strings.Split(clientToken, "Bearer ")
+	if len(splitToken) != 2 {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Invalid token format",
+		})
+	}
+	clientToken = splitToken[1]
+	
+	token, err := jwt.Parse(clientToken, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, &fiber.Error{ Message: "Invalid authorization token"}
+		}
+		return []byte("secret"), nil
+	})
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// store claim in local context
+		c.Locals("userClaims", claims)
+		return c.Next()
+	} else {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "error": "Invalid token claims",
+        })
 	}
 }
 
@@ -39,7 +54,7 @@ func GenerateToken(user *tables.User) (string, error) {
 		"email": user.Email,
 		"name": user.Name,
 		"id": user.ID,
-		"exp": time.Now().Add(time.Minute * 30).Unix(),
+		"exp": time.Now().Add(time.Minute * 1).Unix(),
 	}
 	
 	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte("secret"))
--- routes/route.go
+++ routes/route.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/0xsenzel/go-fiber-boilerplate/internal/controllers/auth"
 	"github.com/0xsenzel/go-fiber-boilerplate/internal/controllers/user"
+	"github.com/0xsenzel/go-fiber-boilerplate/internal/middlewares"
 	"github.com/gofiber/fiber/v3"
 	"gorm.io/gorm"
 )
@@ -12,11 +13,19 @@ func SetupRoutes(app *fiber.App, db *gorm.DB) {
 	app.Post("/user/create", func(c fiber.Ctx) error {
 		return user.CreateUserHandler(c, db)
 	})
-	app.Get("/user/:id", func(c fiber.Ctx) error {		
-		return user.GetUserHandler(c, db)
-	})
 
 	app.Post("auth/login", func(c fiber.Ctx) error {
 		return auth.LoginUserHandler(c, db)
+	})
+
+	protectedRoute(app, db)
+}
+
+func protectedRoute(app *fiber.App, db *gorm.DB) {
+	protected := app.Group("/user", func(c fiber.Ctx) error {
+		return middlewares.JwtAuth(c)
+	})
+	protected.Get("/:id", func(c fiber.Ctx) error {
+		return user.GetUserHandler(c, db)
 	})
 }
